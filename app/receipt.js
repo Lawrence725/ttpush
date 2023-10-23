1479a98ebc6a24b2b990fdebe00c8bf555d0a8cf
Lawrence <lawrence@dotdot.cc>
2023-10-22 23:59:48 -0400

    Add 捐發票+載具設定

---
+++ app/receipt.js
@@ -0,0 +1,14 @@
+// Popup (已提交申請)
+function popSuccess() {
+    var popSuccess  = document.getElementById("popSuccess");
+    popSuccess.style.display = "flex"; }
+
+// Popup (重新綁定)
+function popRenew() {
+    var popRenew  = document.getElementById("popRenew");
+    popRenew.style.display = "flex"; }
+
+// Popup (解除綁定)
+function popDelete() {
+    var popDelete  = document.getElementById("popDelete");
+    popDelete.style.display = "flex"; }
